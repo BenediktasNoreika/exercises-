d384ed1b07bd78ed239b4e4cb72d48cb10c9c69c
BenediktasNoreika <73648975+BenediktasNoreika@users.noreply.github.com>
2020-11-05 11:57:15 +0000

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		number(2,3,true);
+
+	 }
+	
+
+	
+	public static int number(int a, int b, boolean c) {
+	
+	if (c == true) {
+	return a + b;
+	
+	if(c == false) {
+	return a*b;
+	}
+	}
+	}
+}
