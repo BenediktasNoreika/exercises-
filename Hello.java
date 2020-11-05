957bec586dfc2da9b1bbfd9d181732bf089c1fc9
BenediktasNoreika <73648975+BenediktasNoreika@users.noreply.github.com>
2020-11-05 12:57:21 +0000

    Update Hello.java
    Completed flowchart exercise

--- Hello.java
+++ Hello.java
@@ -2,19 +2,54 @@
 public class Hello {
 	public static void main(String[] args) {
 		number(2,3,true);
+		//count();
+		count2(3);
 
 	 }
 	
 
 	
 	public static int number(int a, int b, boolean c) {
-	
+	int ans;
 	if (c == true) {
-	return a + b;
+	ans = a + b;	
+	}
+	else {
+	ans = a*b;
+	}
+	return ans;
+	}
 	
-	if(c == false) {
-	return a*b;
+	
+	public static void count() {
+		
+		for (int i=100;i <200 ;i++ ) {
+			System.out.println(i);
+		}
+		
+		for (int i=100;i <=200 ;i++ ) {
+			if (i%2 == 0) {
+				System.out.println("-");
+			}
+			else {
+				System.out.println("*");
+			}	
+		}
+	  for (int i=0; i <=10; i++) {
+		for (int x=0; x <=10; x++) {
+			System.out.println(x);
+		}
+		}
 	}
+	
+	public static void count2(int num) {
+		for (int i=0; i <num; i++) {
+			for (int x=0; x <=10; x++) {
+				System.out.println(x);
+			}
+			}
 	}
+	
+	
 	}
-}
+
